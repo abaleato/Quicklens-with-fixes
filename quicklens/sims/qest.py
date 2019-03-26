fc4a449c1d660529a75cb562b359e923b11f296f
abaleato <48951377+abaleato@users.noreply.github.com>
2019-03-26 14:54:03 +0000

    Added curved-sky functionality
    Added get_qft_full_sky, get_qr_full_sky and get_fl_full_sky. The latter is very much tentative as it only works in the limit of pure signal and full sky. Needs thorough revisiting.

--- quicklens/sims/qest.py
+++ quicklens/sims/qest.py
@@ -149,6 +149,92 @@ class library():
                 self.qrs[(ke,ks)] = ret
             return self.qrs[(ke,ks)]
 
+    def get_qr_full_sky(self, ke, ks=None, beam_fwhm_rad=10.0, lmax=3000):
+        """ By A.Baleato.
+                ke = estimator key.
+                (optional) ks = source key (defaults to ke).
+        """
+        print 'ooo'
+        if ks == None:
+            ks = ke
+
+        if (isinstance(ke, tuple) and isinstance(ks, tuple)): # a (qest.qest, scaling) pair for both estimator and source keys.
+            qe, f12, f1, f2 = ke
+            qs, f34 = ks
+
+            if f12 == f34:
+                ret = np.zeros(self.cl_len.ls.shape)#length lmax+1, see what qe_cov_fill_helper_fullsky takes in. #ql.maps.cfft(f1.nx, f1.dx, ny=f1.ny, dy=f1.dy)
+                return qe.fill_resp( qs, ret, f1, f2, npad=self.npad)
+            else:
+                #We don't foresee using ke!=ks, so I'm not completely sure about the modification below. It probably won't be used, anyway.
+                return np.zeros(self.cl_len.ls.shape)
+
+        elif isinstance(ke, tuple): # a (qest.qest, scaling) pair only for the estimator. need to expand the source key.
+            qe, f12, f1, f2 = ke
+            
+            ret = np.zeros(self.cl_len.ls.shape) #ql.maps.cfft(f1.nx, f1.dx, ny=f1.ny, dy=f1.dy)
+            for (tqs, tfs) in self.get_qe(ks):
+                ret += self.get_qr_full_sky( ke, ks=tqs) * tfs
+            return ret
+            
+        else: # keys for both the estimator and source. need to expand.
+            if (ke,ks) not in self.qrs.keys():
+                tfl1 = self.get_fl_full_sky('cltt', beam_fwhm_rad, lmax=lmax)
+                efl1 = self.get_fl_full_sky('clee', beam_fwhm_rad, lmax=lmax)
+                bfl1 = self.get_fl_full_sky('clbb', beam_fwhm_rad, lmax=lmax)
+                if self.ivfs2 is not self.ivfs1:
+                    # This if statement is useless unless modified
+                    tfl1 = self.get_fl_full_sky('cltt', beam_fwhm_rad, lmax=lmax)
+                    efl1 = self.get_fl_full_sky('clee', beam_fwhm_rad, lmax=lmax)
+                    bfl1 = self.get_fl_full_sky('clbb', beam_fwhm_rad, lmax=lmax)
+                else:
+                    tfl2, efl2, bfl2 = tfl1, efl1, bfl1
+                
+                ret = np.zeros(self.cl_len.ls.shape) #ql.maps.cfft(tfl1.nx, tfl1.dx, ny=tfl1.ny, dy=tfl1.dy)
+                for tqe, tfe in self.get_qe(ke):
+                    if not isinstance(tqe, tuple):
+                        ret += self.get_qr_full_sky( tqe, ks=ks ) * tfe
+                    else:
+                        qe, f12 = tqe
+            
+                        f1 = {'T' : tfl1, 'E' : efl1, 'B' : bfl1}[f12[0]]
+                        f2 = {'T' : tfl2, 'E' : efl2, 'B' : bfl2}[f12[1]]
+                        
+                        for (tqs, tfs) in self.get_qe(ks):
+                            ret += self.get_qr_full_sky( (qe,f12,f1,f2), ks=tqs ) * tfe * tfs
+                self.qrs[(ke,ks)] = ret
+            return self.qrs[(ke,ks)]
+
+    def get_fl_full_sky(self, which_cl, beam_fwhm_rad, lmax=3000):
+        '''By ABL. IVF and deconvolution. mimics what is done in ivf.py'''
+        print 'yo'
+        #beam_size_arcmin = beam_fwhm_rad * 180./np.pi *60.0 #hp.pixelfunc.nside2resol(nside, arcmin=True) #arcmin
+        #beam = self.bl(beam_size_arcmin, lmax)**2
+
+        lmax_v2 = self.cl_len.ls[-1]
+        assert lmax==lmax_v2
+        #Should I also include beam deconvolution in this? NO!!!!
+        if which_cl=='cltt':
+            #Convert the Cl to K bc thats what the sims are in
+            cl_in_K = self.cl_len.cltt
+        elif which_cl=='clee':
+            cl_in_K = self.cl_len.clee
+        elif which_cl=='clbb':
+            cl_in_K = self.cl_len.clbb
+        else:
+            print 'Error!!'
+        fl = 1./cl_in_K
+        fl[0:2] = 0
+        return fl
+
+    def bl(self, fwhm_arcmin, lmax):
+        """ Put here by ABL. returns the map-level transfer function for a symmetric Gaussian beam.
+            * fwhm_arcmin      = beam full-width-at-half-maximum (fwhm) in arcmin.
+            * lmax             = maximum multipole.
+        """
+        ls = np.arange(0, lmax+1)
+        return np.exp( -(fwhm_arcmin * np.pi/180./60.)**2 / (16.*np.log(2.)) * ls*(ls+1.) )
+
     def get_qft(self, k, tft1, eft1, bft1, tft2, eft2, bft2):
         """ return the estimate for key k.
                 k          = estimator key.
@@ -164,6 +250,26 @@ class library():
         for tqe, tfe in self.get_qe(k):
             if not isinstance(tqe, tuple):
                 ret += self.get_qft( tqe, tft1, eft1, bft1, tft2, eft2, bft2 ) * tfe
+            else:
+                (qe, f12) = tqe
+
+                f1 = {'T' : tft1, 'E' : eft1, 'B' : bft1}[f12[0]]
+                f2 = {'T' : tft2, 'E' : eft2, 'B' : bft2}[f12[1]]
+                ret += qe.eval(f1, f2, npad=self.npad) * tfe
+        return ret
+
+    def get_qft_full_sky(self, k, tft1, eft1, bft1, tft2, eft2, bft2):
+        """ By A.Baleato. return the estimate for key k.
+                k          = estimator key.
+                [t,e,b]ft1 = alm's for teb
+                [t,e,b]ft2 = ''
+                Return the vlm's.
+        """
+        lmax = len(self.cl_unl.cltt)-1
+        ret = np.zeros( (lmax+1)**2, dtype=np.complex )
+        for tqe, tfe in self.get_qe(k):
+            if not isinstance(tqe, tuple):
+                ret += self.get_qft_full_sky( tqe, tft1, eft1, bft1, tft2, eft2, bft2 ) * tfe
             else:
                 (qe, f12) = tqe
 
